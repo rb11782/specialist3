b10c2ce3a8e7cb67391c838351adc7db5d8e234b
Raymond Bertrand <rb11782@gmail.com>
2019-10-16 02:01:22 +0000

    Initial commit

---
+++ finallinkedlist2.rb
@@ -0,0 +1,54 @@
+class LinkedListNode
+  attr_accessor :value, :next_node
+
+  def initialize(value, next_node=nil)
+    @value = value
+    @next_node = next_node
+  end
+end
+
+class Stack
+  attr_reader :data
+
+  def initialize
+    @data = nil
+  end
+
+  def push(value)
+    @data = LinkedListNode.new(value, @data)
+  end
+
+  def pop
+    return print "nil\n" if @data == nil
+    @data = @data.next_node
+  end
+end
+
+node1 = LinkedListNode.new(9)
+node2 = LinkedListNode.new(62, node1)
+node3 = LinkedListNode.new(34, node2)
+
+def print_values(list_node)
+  if list_node
+    print "#{list_node.value} --> "
+    print_values(list_node.next_node)
+  else
+    print "nil\n"
+    return
+  end
+end
+
+def reverse_list(list, previous=nil)
+  new_head = list.next_node
+  list.next_node = previous
+  if new_head
+    reverse_list(new_head, list)
+  else
+    list
+  end
+end
+
+print_values(node3)
+puts "________________________"
+revlist = reverse_list(node3)
+print_values(revlist)
